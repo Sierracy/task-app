e42609e252d579602075fcb738bcb02ceba2644e
Sierra Ellison <sierra@MacBook-Pro.local>
2021-01-21 17:42:09 -0500

    move table view to a seperate bladefile

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,25 +17,28 @@
                             <!-- Task Name field -->
                             <div class="mb-4">
                                 <label for="description" class="sr-only">Description</label>
-                                <textarea name="description" id="description" cols="30" rows="1" class="bg-gray-100 border-2 w-full p-4 rounded-lg" placeholder="Create a task!"></textarea>
+                                <textarea name="description" id="description" cols="30" rows="1" 
+                                    class="bg-gray-100 border-2 w-full p-4 rounded-lg" placeholder="Create a task!">
+                                </textarea>
                             </div>
 
                             <!-- Assigned To field. TODO upgrade to dropdown of users -->
                             <div class="mb-4">
                                 <label for="assigned_to" class="sr-only">Assigned To</label>
-                                <textarea name="assigned_to" id="assigned_to" cols="30" rows="1" class="bg-gray-100 border-2 w-full p-4 rounded-lg" placeholder="Who is responsible?"></textarea>
+                                <textarea name="assigned_to" id="assigned_to" cols="30" rows="1" 
+                                    class="bg-gray-100 border-2 w-full p-4 rounded-lg" placeholder="Who is responsible?">
+                                </textarea>
                             </div>
                             
+                            <!-- Priority Dropdown -->
                             <div class="inline-block mb-4">
-                                <!-- Priority Dropdown -->
                                 <label for="priority" class="sr-only"></label>
                                 <select class="rounded" name='priority'>
                                     <option selected disabled>Priority</option>
                                     <option value="Low">Low</option>
                                     <option value="Medium">Medium</option>
                                     <option value="High">High</option>
-                                    <option value="Critical">Critical</option>
-                                
+                                    <option value="Critical">Critical</option>                               
                                 </select>
                             
                                 <!-- Due Date Picker -->
@@ -50,61 +53,8 @@
                             </div>
                         </form>
 
-                        @if ($tasks->count())
-                        <div class="overflow-x-auto mt-6">
-                            <table class="table-auto border-collapse w-full">
-                                <thead>
-                                    <tr class="font-medium text-gray-700 text-left bg-gray-200">
-                                        <th class="px-4 py-2">Assigned</th>
-                                        <th class="px-4 py-2">Task</th>
-                                        <th class="px-4 py-2">Due</th>
-                                        <th class="px-4 py-2">Status</th>
-                                        <th class="px-4 py-2">Priority</th>
-                                        <th class="px-4 py-2">Created</th>
-                                        <th class="px-4 py-2"></th>                          
-                                        <th class="px-4 py-2">Updated</th>
-                                        <th class="px-4 py-2"></th>
-                                        <th class="px-4 py-2"></th>
-                                        <th class="px-4 py-2"></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($tasks as $task)
-                                        <tr class="border-b border-gray-200 py-10 text-sm">
-                                            <td class="px-4 py-2">{{ $task->assigned_to }}</td>
-                                            <td class="px-4 py-2">{{ $task->description }}</td>
-                                            <td class="px-4 py-2">{{ \Carbon\Carbon::createFromFormat('Y-m-d', $task->due_date)->format('F j, Y ') }}</td>
-                                            <td class="px-4 py-2">{{ $task->status }}</td>
-                                            <td class="px-4 py-2">{{ $task->priority }}</td>
-                                            <td class="px-4 py-2 whitespace-nowrap">{{ $task->created_at->format('g:i a') }}</td>
-                                            <td class="px-4 py-2 whitespace-nowrap">{{ $task->created_at->format('F j, Y') }}</td>
-                                            
-                                            @if ( !$task->updated_at->equalTo($task->created_at) )
-                                            <td class="px-4 py-2 whitespace-nowrap">{{ $task->updated_at->format('g:i a') }}</td>
-                                            <td class="px-4 py-2 whitespace-nowrap">{{ $task->updated_at->format('F j, Y') }}</td>
+                        <x-taskTable :tasks="$tasks"></x-taskTable>
 
-                                            @else
-                                            <td class="px-4 py-2 text-center">-</td>
-                                            <td class="px-4 py-2 text-center">-</td>
-                                            @endif
-
-                                            <td class="px-4 py-2">
-                                                <form action="{{ route('task.destroy', $task->id) }}" method="post">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="underline">Delete</button>
-                                                </form>
-                                            </td>
-                                            <td class="px-4 py-4"><a href="{{ route('edit', $task) }}" class="underline">Edit</a></td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            {{$tasks->links()}}
-                        @else
-                            <p>There are no tasks.</p>
-                        @endif
-                        </div>
                     </div>
                 </div>
             </div>
